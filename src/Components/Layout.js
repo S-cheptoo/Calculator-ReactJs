e777dd5f035633c0b0cd44dbbec0fae590d7fccf
Sandra <cheptoosandra.t@gmail.com>
2022-02-26 15:48:23 +0300

    Handled the button-click events

--- src/Components/Layout.js
+++ src/Components/Layout.js
@@ -1,9 +1,82 @@
-import React from "react";
+import React, { useState}from "react";
+import "./Layout.css";
+import Output from "./Output";
 
-const Layout = () => {
+const Layout = (props) => {
+  let [input, setInput] = useState('0')
+  let [result, setResult] = useState('')
+  // event for when a button is clicked
+  const handleClick = (event) => { 
+    const value = event.target.value;
+    if (value === '=') {
+      if (input !== '') {
+        let res = ''
+        try {
+            res = eval(input)
+        } catch (err) {
+          setResult('MathError')
+        }
+        if (res === undefined) {
+          setResult('MathError')
+        }
+        else {
+          setResult(input + '=')
+          setInput(res)
+        }
+      }
+    }
+    // resetting the screen
+    else if (value === 'C') {
+      setInput('0')
+      setResult('')
+    }
+    else if (value === 'DEL') {
+      // manipulating the strings on the screen
+      let str = input
+      str = str.substr(0, str.length - 1)
+      setInput(str)
+    }
+    else if (value === '0') {
+      setInput(value)
+    }
+    else {
+        setInput((input +=value))
+    }
+  };
   return (
-    <div>Layout</div>
+      <div className="frame">
+          <div className="calculator">
+        <br></br>
+        <Output user={input} answer={result}/>
+            {/* <img/> */}
+            <div className="keys">
+                  <input type="button" value={'C'} className="button clear" onClick={handleClick}></input>
+                  <input type="button" value={'DEL'} className="button clear" onClick={handleClick}></input>
+                  <input type="button" value={'%'} className="button operator" onClick={handleClick}></input>
+                  <input type="button" value={'/'} className="button operator" onClick={handleClick}></input>
+
+                  <input type="button" value={'7'} className="button" onClick={handleClick}></input>
+                  <input type="button" value={'8'} className="button" onClick={handleClick}></input>
+                  <input type="button" value={'9'} className="button" onClick={handleClick}></input>
+                  <input type="button" value={'*'} className="button operator" onClick={handleClick}></input>
+
+                  <input type="button" value={'4'} className="button" onClick={handleClick}></input>
+                  <input type="button" value={'5'} className="button" onClick={handleClick}></input>
+                  <input type="button" value={'6'} className="button" onClick={handleClick}></input>
+                  <input type="button" value={'-'} className="button operator" onClick={handleClick}></input>
+
+                  <input type="button" value={'1'} className="button" onClick={handleClick}></input>
+                  <input type="button" value={'2'} className="button" onClick={handleClick}></input>
+                  <input type="button" value={'3'} className="button" onClick={handleClick}></input>
+                  <input type="button" value={'+'} className="button operator" onClick={handleClick}></input>
+
+                  <input type="button" value={'0'} className="button" onClick={handleClick}></input>
+                  <input type="button" value={'.'} className="button" onClick={handleClick}></input>
+                  <input type="button" value={'='} className="button equal-sign" onClick={handleClick}></input>
+            </div>
+          </div>
+    </div>
   )
 }
 
-export default Layout
+export default Layout;
